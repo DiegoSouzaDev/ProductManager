6e385a5814a8e69c645db6f67037c02486bba340
Diego.Souza.Dev <diego.souza.dev@gmail.com>
2018-09-09 22:06:21 -0300

    fix front

--- src/main/resources/static/js/app/CountryController.js
+++ src/main/resources/static/js/app/CountryController.js
@@ -3,32 +3,36 @@
 var module = angular.module('app.controllers', []);
 module.controller("CountryController", [ "$scope", "CountryService",
 		function($scope, CountryService) {
-			$scope.country = {
-				id : null,
-				name : null,
-				abrev : null
-			};
+	var that = this;
+			// $scope.country = {
+			// id : null,
+			// name : null,
+			// abrev : null
+			// };
+			//			
+			// CountryService.getCountryById(1)
+			// .then(function(value) {
+			// console.log(value.data);
+			// }, function(reason) {
+			// console.log("error occured");
+			// }, function(value) {
+			// console.log("no callback");
+			// });
+
+			$scope.loadCountries =  CountryService.getAllCountries().then(function(value) {
+					$scope.allCountries = value.data;
+				}, function(reason) {
+					console.log("error occured");
+				}, function(value) {
+					console.log("no callback");
+				});
 			
-			CountryService.getCountryById(1)
-			.then(function(value) {
-				console.log(value.data);
-			}, function(reason) {
-				console.log("error occured");
-			}, function(value) {
-				console.log("no callback");
-			});
 
 			$scope.saveCountry = function() {
+				console.log("entrou no save");
+				console.log($scope.country);
 				CountryService.saveCountry($scope.country).then(function() {
-					console.log("works");
-					CountryService.getAllCountries().then(function(value) {
-						$scope.allCountries = value.data;
-					}, function(reason) {
-						console.log("error occured");
-					}, function(value) {
-						console.log("no callback");
-					});
-
+					that.loadCountries();
 					$scope.country = {
 						id : null,
 						name : null,
--- src/main/resources/static/js/app/CountryService.js
+++ src/main/resources/static/js/app/CountryService.js
@@ -4,13 +4,13 @@ angular.module('app.services', []).factory('CountryService',
 		[ "$http", function($http) {
 			var service = {};
 			service.getCountryById = function(countryId) {
-				return $http.get(/country/ + countryId);
+				return $http.get("/country/" + countryId);
 			}
 			service.getAllCountries = function() {
-				return $http.get(/country/);
+				return $http.get("/country/");
 			}
 			service.saveCountry = function(country) {
-				return $http.post(/country/, country);
+			return $http.post("/country/", country);
 			}
 			return service;
 		} ]);
